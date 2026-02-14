6bd6f4be35255d0be4ddcc1fd6aac355ad71ea17
arilodeodato <arilodeodato@gmail.com>
2026-02-14 12:18:56 -0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// Isso garante que o React seja encontrado globalmente pelo navegador
+window.React = React;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
